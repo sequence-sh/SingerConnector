2158bd38f3b941669e4c9bd650e17e71697c0896
Mark Wainwright <wainwrightml@gmail.com>
2022-01-31 18:11:01 +0000

    Added Connector Injeection Test

---
+++ Singer.Tests/ConnectorInjectionTest.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Abstractions;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Reductech.Sequence.Connectors.Singer.Tests;
+
+public class ConnectorInjectionTest
+{
+    [Fact]
+    public void TestConnectorInjection()
+    {
+        var ci       = new ConnectorInjection();
+        var contexts = ci.TryGetInjectedContexts();
+        contexts.ShouldBeSuccessful();
+
+        contexts.Value.Should().HaveCount(1);
+
+        var pair = contexts.Value.Single();
+        pair.Name.Should().Be("Singer.FileSystem");
+        pair.Context.Should().BeOfType<FileSystem>();
+    }
+}
